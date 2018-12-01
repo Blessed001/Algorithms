6ef77457e40ec7873f0d6b0954306ba9d8d53ed0
diotec <claudiojoc46@mail.ru>
2018-12-01 06:53:10 +0300

    Sequencial search

--- SequentialSearch/SequentialSearch/Program.cs
+++ SequentialSearch/SequentialSearch/Program.cs
@@ -6,7 +6,12 @@ namespace SequentialSearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] Seq = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int n = Seq.Length;
+            SequentialSearch S = new SequentialSearch(Seq, n, 0);
+
+            Console.WriteLine(S.SeqSearch());
+            Console.ReadKey();
         }
     }
 }
---
+++ SequentialSearch/SequentialSearch/SequentialSearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequentialSearch
+{
+    public class SequentialSearch
+    {
+        private int [] seq;
+        private int n;
+        private int value;
+        public SequentialSearch(int [] S,int N, int V)
+        {
+            seq = S;
+            n = N;
+            value = V;
+        }
+
+        //Sequencial search
+        public int SeqSearch()
+        {
+            for(int i = 0; i < n; i++)
+            {
+                if(seq[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+    }
+}
