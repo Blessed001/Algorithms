23a5c12b237698cafc27a52a8c6733ccf898aa98
diotec <claudiojoc46@mail.ru>
2018-12-05 09:36:43 +0300

    First version of ternary search

---
+++ TernarySearch/TernarySearch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TernarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] seq = { 3, 7, 25, 26, 28, 30, 34, 42, 44, 50, 60, 62, 75, 86, 99, 125, 145, 209, 250 };
+            int n = seq.Length;
+            int v = 7;
+            TernarySearch T = new TernarySearch(seq, n, v);
+            Console.WriteLine(T.Search1());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TernarySearch/TernarySearch/TernarySearch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TernarySearch
+{
+    public class TernarySearch
+    {
+        int[] seq;
+        int N;
+        int Value;
+        public TernarySearch(int[] s, int n, int v)
+        {
+            seq = s;
+            N = n;
+            Value = v;
+        }
+        //First version 
+        public int Search1()
+        {
+            int min = 0;
+            int max = N - 1;
+            int fpivot;
+            int spivot;
+
+            while (min <= max)
+            {
+                int nelem = max - min + 1;
+                if(nelem % 3 == 0)
+                {
+                    fpivot = min + (nelem / 3) - 1;
+                    spivot = min + (2 * nelem / 3) - 1;
+                }
+                else
+                {
+                    fpivot = min + (nelem / 3);
+                    spivot = min + (2 * nelem / 3);
+                }
+                if(seq[fpivot] == Value)
+                {
+                    return seq[fpivot];
+                }
+                else if(seq[fpivot] > Value)
+                {
+                    max = fpivot - 1;
+                }
+                else if(seq[spivot] == Value)
+                {
+                    return seq[spivot];
+                }
+                else if(seq[spivot]> Value)
+                {
+                    min = fpivot + 1; max = spivot - 1;
+                }
+                else
+                {
+                    min = spivot + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
